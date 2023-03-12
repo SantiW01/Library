eacada69f29874a16901f26ee5d5c6e40e8601f4
SantiW01 <sweihmuller01@gmail.com>
2023-03-12 17:42:20 -0300

    feat: loop throught array nd display each book

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ function Book(name, author, pages, alreadyRead) {
 function addBookToLibrary(book) {
   mylibrary.push(book);
 }
+
+function displayBooks(arrayBook) {
+  const table = document.querySelector("tbody");
+  for (let i = 0; i < arrayBook.length; i++) {
+    const newTr = document.createElement("tr");
+    table.appendChild(newTr);
+    const thName = document.createElement("th");
+    thName.innerHTML = `${arrayBook[i].author}`;
+    newTr.appendChild(thName);
+    const thAuthor = document.createElement("th");
+    thAuthor.innerHTML = `${arrayBook[i].author}`;
+    newTr.appendChild(thAuthor);
+    const thPages = document.createElement("th");
+    thPages.innerHTML = `${arrayBook[i].pages}`;
+    newTr.appendChild(thPages);
+    const thRead = document.createElement("th");
+    thRead.innerHTML = `${arrayBook[i].alreadyRead}`;
+    newTr.appendChild(thRead);
+  }
+  mylibrary.splice(0, mylibrary.length);
+}
